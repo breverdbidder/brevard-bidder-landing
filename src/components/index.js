94a227ce89282a392dec47624e5ea9618b55af82
breverdbidder <brevardbidderai@gmail.com>
2025-12-08 08:45:05 -0500

    📦 Add components index

---
+++ src/components/index.js
@@ -0,0 +1,7 @@
+// BrevardBidderAI Component Exports
+// Author: Ariel Shapira, Everest Capital USA
+
+export { AuctionMap } from './AuctionMap';
+export { ChatPanel } from './ChatPanel';
+export { PropertyDrawer } from './PropertyDrawer';
+export { SplitScreenLayout } from './SplitScreenLayout';
