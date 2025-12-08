b5e8e33d21d788fe4105b6b22690453ccd1bee4e
breverdbidder <brevardbidderai@gmail.com>
2025-12-08 11:53:11 -0500

    🎨 Shadcn: skeleton component

---
+++ components/ui/skeleton.tsx
@@ -0,0 +1,19 @@
+// Shadcn UI Skeleton Component
+// BrevardBidderAI - Loading States
+// Author: Ariel Shapira, Solo Founder, Everest Capital USA
+
+import { cn } from "@/lib/utils"
+
+function Skeleton({
+  className,
+  ...props
+}: React.HTMLAttributes<HTMLDivElement>) {
+  return (
+    <div
+      className={cn("animate-pulse rounded-md bg-gray-700", className)}
+      {...props}
+    />
+  )
+}
+
+export { Skeleton }
