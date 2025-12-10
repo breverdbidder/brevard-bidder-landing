1b9855e2149a9bf85a16de68726f43cd30d5cf6c
breverdbidder <brevardbidderai@gmail.com>
2025-12-10 10:46:02 -0500

    Export ScraperStatus component for chat interface

--- src/components/index.js
+++ src/components/index.js
@@ -1,7 +1,8 @@
 // BrevardBidderAI Component Exports
 // Author: Ariel Shapira, Everest Capital USA
 
-export { AuctionMap } from './AuctionMap';
-export { ChatPanel } from './ChatPanel';
-export { PropertyDrawer } from './PropertyDrawer';
-export { SplitScreenLayout } from './SplitScreenLayout';
+export { AuctionMap } from "./AuctionMap";
+export { ChatPanel } from "./ChatPanel";
+export { PropertyDrawer } from "./PropertyDrawer";
+export { SplitScreenLayout } from "./SplitScreenLayout";
+export { default as ScraperStatus } from "./ScraperStatus";
