ba61998f9a297eab63659deccf54e3b3f872baa7
breverdbidder <brevardbidderai@gmail.com>
2025-12-28 13:22:56 -0500

    feat: Export BidDeedSplitScreen

--- src/components/index.js
+++ src/components/index.js
@@ -3,6 +3,7 @@
 
 export { AuctionMap } from "./AuctionMap";
 export { default as BidDeedMap } from "./BidDeedMap";
+export { default as BidDeedSplitScreen } from "./BidDeedSplitScreen";
 export { ChatPanel } from "./ChatPanel";
 export { PropertyDrawer } from "./PropertyDrawer";
 export { SplitScreenLayout } from "./SplitScreenLayout";
