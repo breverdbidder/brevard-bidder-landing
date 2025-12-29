12b7d3b5c0bda2c5dc086ba88ed1d7bc909b65f1
breverdbidder <brevardbidderai@gmail.com>
2025-12-29 06:36:30 -0500

    Add data visualization page

---
+++ pages/data.jsx
@@ -0,0 +1,8 @@
+import React from 'react';
+import AuctionDataDashboard from '../src/components/AuctionDataDashboard';
+
+const DataPage = () => {
+  return <AuctionDataDashboard />;
+};
+
+export default DataPage;
